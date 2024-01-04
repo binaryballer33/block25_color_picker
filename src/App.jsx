2c4d3ce557ab5ce2d2a38b471c4a82cfadd26cb2
Shaquille Mandy <shaqmandy@gmail.com>
2024-01-04 15:57:04 -0300

    implemented color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 // Write your Color component here
+// eslint-disable-next-line react/prop-types
+const Color = ({ color, colorSetter }) => {
+
+  return (
+    <div 
+      className={color}
+      onClick={() => colorSetter(color)}
+    >
+      <p style={{ color: "white", textAlign: 'center', padding: '25px'}}>
+        {color}
+      </p>
+    </div>
+  );
+};
+
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState(null);
+  
   return (
     <div id="container">
-      <div id="navbar">
+      <div id="navbar" className={selectedColor}>
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div>{selectedColor}</div>
       </div>
       <div id="colors-list">{/* colors go here */}</div>
+
+      <div id="colors-list">
+        <Color color="blue" colorSetter={setSelectedColor}/>
+        <Color color="violet" colorSetter={setSelectedColor}/>
+        <Color color="black" colorSetter={setSelectedColor}/>
+      </div>
     </div>
   );
 };
